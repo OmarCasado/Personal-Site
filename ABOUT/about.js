8749e60975f4278905b50a4443b5cc2beb8baee3
Omar Casado <omarcasado27@hotmail.com>
2023-06-11 17:11:31 +0900

    Personal Site Initial Commit

---
+++ ABOUT/about.js
@@ -0,0 +1,30 @@
+
+
+
+// HERO SECTION BACKGROUND TRANSITIONS
+
+function mouseOver() {
+    document.getElementById("hero").style.backgroundImage = " url('../Images/Karesansui\ background\ Architecture\ button.png')";
+}
+
+function mouseOut() {
+document.getElementById("hero").style.backgroundImage =  " url('../Images/Karesansui\ Background.png')";
+}
+
+function mouseUp() {
+    document.getElementById("hero").style.backgroundImage = " url('../Images/Karesansui\ background\ Web-Dev\ button.png')";
+}
+
+function mouseDown() {
+document.getElementById("hero").style.backgroundImage =  " url('../Images/Karesansui\ Background.png')";
+}
+
+// FINAL SECTION BACKGROUND TRANSITIONS
+
+function mouseOn() {
+    document.querySelector(".final-container").style.backgroundImage = " url('../Images/Karesansui final background Reach-Out button.png')";
+}
+
+function mouseOff() {
+document.querySelector(".final-container").style.backgroundImage =  " url('../Images/Karesansui final background.jpg')";
+}
