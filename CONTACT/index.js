ae719d777cbadc8d28f804e1e61c223e3bb0fec5
Omar Casado <omarcasado27@hotmail.com>
2023-06-14 19:09:05 +0900

    Fix contact page title to change after user prompt.

--- CONTACT/index.js
+++ CONTACT/index.js
@@ -1,4 +1,10 @@
 
-$("#name").click(function(event){
-    $("h1").text("Hi!");
+document.addEventListener('DOMContentLoaded', function() {
+    var nameInput = document.getElementById('name');
+    var sectionTitle = document.querySelector('.section-title');
+
+    nameInput.addEventListener('input', function() {
+    var name = nameInput.value;
+    sectionTitle.textContent = "Hi " + name + "!";
+    });
 });
