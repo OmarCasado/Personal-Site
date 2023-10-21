4c7131f1c4952c024ddd9eefd2da29bd9b3d9e19
Omar Casado <omarcasado27@hotmail.com>
2023-10-21 23:30:14 +0900

    Reorganize folders names.

---
+++ javascript/index.js
@@ -0,0 +1,32 @@
+
+
+
+// HERO SECTION BACKGROUND TRANSITIONS
+
+function mouseOver() {
+    document.getElementById("hero").style.backgroundImage = " url('images/Karesansui\ background\ Architecture\ button.png')";
+}
+
+function mouseOut() {
+document.getElementById("hero").style.backgroundImage =  " url('images/Karesansui\ Background.png')";
+}
+
+function mouseUp() {
+    document.getElementById("hero").style.backgroundImage = " url('images/Karesansui\ background\ Web-Dev\ button.png')";
+}
+
+function mouseDown() {
+document.getElementById("hero").style.backgroundImage =  " url('images/Karesansui\ Background.png')";
+}
+
+// FINAL SECTION BACKGROUND TRANSITIONS
+
+function mouseOn() {
+    document.querySelector(".final-container").style.backgroundImage = " url('images/Karesansui final background Reach-Out button.png')";
+}
+
+function mouseOff() {
+document.querySelector(".final-container").style.backgroundImage =  " url('images/Karesansui final background.jpg')";
+}
+
+
